80aa5bd126e613547d4646e7e0f236a73efcb2f1
giorgtarkha <giorgi.tarkhnishvili123@gmail.com>
2021-02-15 01:19:10 +0400

    Added restart button from pause

--- js/pause.js
+++ js/pause.js
@@ -1,11 +1,18 @@
-let pause_continue_button, pause_exit_button;
+let pause_continue_button;
+let pause_exit_button;
+let pause_restart_game_button;
 
 let init_pause = () => {
 	pause_continue_button = document.getElementById("continue-game-button");
+	pause_restart_game_button = document.getElementById("pause-restart-game-button");
 	pause_exit_button = document.getElementById("exit-game-button");
 	
 	pause_continue_button.addEventListener("click", () => {
 		load_start_game_scene();
+	});
+	
+	pause_restart_game_button.addEventListener("click", () => {
+		load_start_game_scene(true);
 	});
 	
 	pause_exit_button.addEventListener("click", () => {
--- js/scene_manager.js
+++ js/scene_manager.js
@@ -79,6 +79,7 @@ let load_map_choose_scene = () => {
 		remove_song_controls();
 		change_current_map_image();
 		hide_gameplay_ui();
+		song_player.play();
 	} else if (current_scene == game_result_scene) {
 		reset_game();
 		current_scene = map_choose_scene;
@@ -97,17 +98,24 @@ let load_pause_game_scene = () => {
 	}
 }
 
-let load_start_game_scene = () => {
+let load_start_game_scene = (restart = false) => {
 	if (current_scene == map_choose_scene) {
 		current_scene = playing_game_scene;
 		load_scenes([playing_game_scene]);
 		init_game();
 		show_gameplay_ui();
 	} else if (current_scene == pause_game_scene) {
-		current_scene = playing_game_scene;
-		load_scenes([playing_game_scene]);
-		song_player.play();
-		continue_game();
+		if (restart) {
+			reset_game();
+			current_scene = playing_game_scene;
+			load_scenes([playing_game_scene]);
+			init_game();
+		} else {
+			current_scene = playing_game_scene;
+			load_scenes([playing_game_scene]);
+			song_player.play();
+			continue_game();
+		}
 	} else if (current_scene == game_result_scene) {
 		reset_game();
 		current_scene = playing_game_scene;
