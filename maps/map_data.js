be6a1bc434f0d5ea4ae32d6d54acefcff6e6fdf8
giorgtarkha <giorgi.tarkhnishvili123@gmail.com>
2021-02-15 01:39:54 +0400

    made each map have at least one circle

--- maps/map_data.js
+++ maps/map_data.js
@@ -24,7 +24,10 @@ map_data["Naruto Shippuden OP3"] = {
 	original_screen_width: 1920,
 	original_screen_height: 1080,
 	flow: [
-		
+		{ time : 500, x : 500, y : 200},
+		{ time : 700, x : 600, y : 700 },
+		{ time : 800, x : 600, y : 500 },
+		{ time : 1200, x : 500, y : 500 },
 	]
 }
 		
@@ -35,6 +38,13 @@ map_data["Hunter X Hunter OP1"] = {
 	original_screen_width: 1920,
 	original_screen_height: 1080,
 	flow: [
-		
+		{ time : 500, x : 500, y : 200},
+		{ time : 900, x : 1200, y : 800 },
+		{ time : 1300, x : 600, y : 500 },
+		{ time : 1900, x : 500, y : 500 },
+		{ time : 2300, x : 1000, y : 800 },
+		{ time : 2600, x : 1100, y : 900 },
+		{ time : 2900, x : 250, y : 500 },
+		{ time : 3800, x : 300, y : 900 },
 	]
 }
